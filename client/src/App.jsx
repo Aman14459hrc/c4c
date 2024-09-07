9fc5af435b079ae9791d3446867e932b76a1a5d9
Aman14459hrc <mr.aman2021@gmail.com>
2024-09-07 17:35:57 +0530

    git amn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Login from './components/Login'
+import home from './components/home'
+//import {BrowserRouter as Router}, Routes, Route from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <Login/>
+      <home/>
     </>
   )
 }
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React from 'react';
+//import Box from '@mui/material/Box';
+//import TextField from '@mui/material/TextField';
 
-export const home = () => {
-  return <>
-    <div>home</div>
-  </>
-  
-}
+const Home = () => {
+    return (
+        <div>
+            <div>home</div>
+            <div className='about'>
+                <h2 className='h2'>
+                    About us
+                </h2>
+                {/* Add your content for the "About us" section here */}
+                {/* For example, you can add a paragraph or other components */}
+                <p>We are a passionate team committed to making the world a greener place!</p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
